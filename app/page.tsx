c692628ad5bb4ac0a4d9d7d96e4a48c60a6b1aac
Ajungmiiin <wjdals3249@naver.com>
2026-05-02 19:28:02 +0900

    feat : add suspense for service tab component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import { PainPoints } from '@/components/landing/pain-points'
 import { CompanyIntro } from '@/components/landing/company-intro'
 import { ContactForm } from '@/components/landing/contact/contact-form'
 import ServiceTab from '@/components/landing/service/service-tab'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <ServiceTab />
+      <Suspense fallback={null}>
+        <ServiceTab />
+      </Suspense>
       <PainPoints />
       <CompanyIntro />
       <ContactForm />
